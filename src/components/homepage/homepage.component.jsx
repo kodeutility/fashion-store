ab32961db187f6d158692ede4f53097f74d891af
Kiran <kodeutility@gmail.com>
2019-11-09 23:21:38 +0530

    replaced code with Directory component

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,43 +1,10 @@
 import React from "react";
+import Directory from "../directory/directory.component";
 import "./homepage.styles.scss";
 
 const Homepage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="title">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="title">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="title">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN</h1>
-          <span className="title">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN</h1>
-          <span className="title">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
